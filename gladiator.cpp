bf65fff0056cb006aa5452c7da1faac97ba1f918
Ryan Houck <ryan_houck@taylor.edu>
2019-01-07 18:54:19 -0500

    Got the text test working.

--- gladiator.cpp
+++ gladiator.cpp
@@ -4,15 +4,14 @@
 
 using namespace std;
 
-string getInput(string question)
+string getInput(string question, int isCaps = 0)
 {
     string input = "";
-    cout << question << endl
-         << "> ";
+    cout << question << endl;
     cin >> input;
-    for (unsigned long int i = 0; i < input.length(); i++)
+    for (unsigned long int i = 0; (i < input.length()); i++)
     {
-        if (input[i] >= 65 && input[i] <= 90)
+        if (input[i] >= 65 && input[i] <= 90 && !isCaps)
         {
             input[i] += 32;
         }
@@ -22,15 +21,24 @@ string getInput(string question)
 
 int main()
 {
-    string name = getInput("Welcome to The Arena! Tell me, adventurer, what's your name?");
+    string name = getInput("Welcome to The Arena! Tell me, adventurer, what's your name?", 1);
+    string style;
+    string race;
     while (name.length() == 0)
     {
         name = getInput("Come on now, don't be shy! Tell me what your name is!");
     }
-    string input = getInput("Very well then, " + name + ". Are you a mage, a warrior, or a dualfighter?");
-    while (input.compare("warrior") != 0 && input.compare("mage") != 0 && input.compare("dualfighter"))
+    style = getInput("Very well then, " + name + ". Are you a mage, a warrior, or a dualfighter?");
+    while (style.compare("warrior") != 0 && style.compare("mage") != 0 && style.compare("dualfighter"))
     {
-        getInput("I've never heard of a " + input + "! Are you a mage, a warrior, or a dualfighter?");
+        style = getInput("I've never heard of a " + style + "! Are you a mage, a warrior, or a dualfighter?");
     }
+    race = getInput("OK, then. What's your race?");
+    while (race.compare("human") != 0 && race.compare("dwarf") != 0 && race.compare("dwarf") != 0)
+    {
+        race = getInput("Don't lie to me, I can clearly tell what you aren't a " + race + "! Now, what race are you?");
+    }
+    cout << name << ", the " << race << " " << style << endl;
+
     return 0;
 }
